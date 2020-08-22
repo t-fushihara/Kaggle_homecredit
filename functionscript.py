65feab5302708b6ea2752b029d841eb5cc2db928
github t-fushihara <shake.ha.delicious@gmail.com>
2020-08-22 15:52:57 +0900

    0822 ベースモデル作成といくつか特徴量の追加

--- functionscript.py
+++ functionscript.py
@@ -152,3 +152,10 @@ def iplt_countplot(var_name,tar_name,data):
     #プロットを行う
     fig = go.Figure(data=fig_data, layout=layout)
     iplot(fig)
+
+def one_hot_encoder(df, nan_as_category = True):
+    original_columns = list(df.columns)
+    categorical_columns = [col for col in df.columns if df[col].dtype == 'object']
+    df = pd.get_dummies(df, columns= categorical_columns, dummy_na= nan_as_category)
+    new_columns = [c for c in df.columns if c not in original_columns]
+    return df, new_columns
